3084d65c6b7a971c005278b8c34def8c9b4dcace
Susanta Nahak <susant.nahak@kousel.io>
2023-10-15 09:36:20 +0530

    Initial Commit

---
+++ SwiftUIDemoProjects/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIDemoProjects
+//
+//  Created by Susant Nahak on 15/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDemoProjects/SwiftUIDemoProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoProjectsApp.swift
+//  SwiftUIDemoProjects
+//
+//  Created by Susant Nahak on 15/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
